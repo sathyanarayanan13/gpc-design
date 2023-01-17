8486733bbff0c6acfc8e6de61a688754fb784d5f
sathyanarayanan13 <sathya.naran05@gmail.com>
2023-01-18 00:33:21 +0530

    first commit

--- src/components/MenuComponent.jsx
+++ src/components/MenuComponent.jsx
@@ -9,7 +9,7 @@ export default class MenuComponent extends Component {
                 <ul className="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
                     <li className="nav-item">
                     <a href="#" className="nav-link">
-                        <p> CCV </p>
+                        <p>CCV</p>
                     </a>
                     </li>
                 </ul>
